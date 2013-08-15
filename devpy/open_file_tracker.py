d5e17fb594b4ca4e3e8c1360b96e6c960ea84cd0
basvandenberg <b.a.vandenberg@gmail.com>
2013-08-15 08:43:32 +0200

    Added comments

--- devpy/open_file_tracker.py
+++ devpy/open_file_tracker.py
@@ -9,18 +9,27 @@ class NewFile(oldfile):
 
     def __init__(self, *args):
 
-        self.x = args[0]
+        # store the file name
+        self.file_path = args[0]
 
-        print("==> open %s" % str(self.x))
+        # print that this file is going to be opened
+        print("==> open %s" % str(self.file_path))
 
+        # use builtin file to open file
         oldfile.__init__(self, *args)
+
+        # add this file to the set of open files
         openfiles.add(self)
 
     def close(self):
 
+        # print that this file is going to be closed
         print("==> close %s" % str(self.x))
 
+        # use builtin file to close the file
         oldfile.close(self)
+
+        # remove this file from the set of open files
         openfiles.remove(self)
 
 oldopen = __builtin__.open
@@ -37,5 +46,9 @@ __builtin__.open = newopen
 
 
 def print_open_files():
+    '''
+    This function prints how many files are currently opened to the standard
+    output, followed by a list with names of the open files.
+    '''
     print("==> %d open files: [%s]" %
           (len(openfiles), ", ".join(f.x for f in openfiles)))
